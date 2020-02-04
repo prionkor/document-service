7d00cf5c9dc90a1c5430d2bb237d0adf9b4f613f
Partha Sarathi Acharjya <39263124+partha-uiu@users.noreply.github.com>
2020-02-04 17:53:02 +0600

    PDF stream created with html-pdf

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const pdf = require('html-pdf');
 const fs = require('fs');
 const bodyParser= require('body-parser')
 const app = express()
+const cors = require('cors');
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser());
 
-
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('hello');
@@ -16,33 +17,26 @@ app.get('/', (req, res) => {
 app.post('/document/pdf', (req, res) => {
    
     const html = req.body.html;
-
-    // // pdf.create(html, options).toFile("report.pdf", function (err, html) {
-    // //     if (err) {
-    // //         res.send(err);
-    // //     } else {
-    // //         res.send("File created successfully");
-    // //     }
-    // // });
-
-
-    // pdf.create(html).toStream(function (err, stream) {
-    //     stream.pipe(fs.createWriteStream('./foo.pdf'));
-    // });
+    console.log(" fdfdfdferror");
     var options = { format: 'Letter' };
- 
+    
+
     pdf.create(html).toStream(function(err, stream) {
         if (err) {
-            console.log(err)
+            res.send(404);
         } else {
-            res.set('Content-type', 'application/pdf');
-            stream.pipe(res)
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', 'attachment; filename=test-file.pdf;');
+
+            stream.pipe(res);
+
+          
         }
     });
 
 });
 
 
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(8000, () => {
+    console.log("Server is listening on port 8000");
 });
